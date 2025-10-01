aecd520721e65452561abb5f031269fceaf4cc1e
fonsii <abeaalfonso@gmail.com>
2025-10-01 12:30:50 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import Home from "./Webpage/home";
 import Projects from "./Webpage/upload";
 import Social from "./Webpage/signin";
 
-function App () {
+function App() {
     return (
         <>
-        
-        <Router>
-            <Routes>
-                {/*This will link to the Feed */}
-                <Route path="/" element={<Home/>} />
 
-                {/*This will link  to the ProjectPage */}
-                <Route path="/projects" element={<Projects/>} />
+            <Router>
+                <Routes>
+                    {/*This will link to the Feed */}
+                    <Route path="/" element={<Home />} />
 
-                {/*This will link to the Sign In page */}
-                <Route path="/connecting" element={<Social/>} />
-            </Routes>
-        </Router>
+                    {/*This will link  to the ProjectPage */}
+                    <Route path="/projects" element={<Projects />} />
+
+                    {/*This will link to the Sign In page */}
+                    <Route path="/connecting" element={<Social />} />
+                </Routes>
+            </Router>
         </>
     );
 }
